cab55f829cda8c303d992d0aa644fa4c99a9ba1b
bgk <bgk083@163.com>
2017-05-21 20:14:53 +0800

    modify assert equal poll message and local message

--- src/main/java/io/openmessaging/demo/DemoTester.java
+++ src/main/java/io/openmessaging/demo/DemoTester.java
@@ -19,7 +19,7 @@ public class DemoTester {
         //实际测试时利用 STORE_PATH 传入存储路径
         //所有producer和consumer的STORE_PATH都是一样的，选手可以自由在该路径下创建文件
          */
-        properties.put("STORE_PATH", "/home/admin/test");
+        properties.put("STORE_PATH", "/home/bgk/test");
 
         //这个测试程序的测试逻辑与实际评测相似，但注意这里是单线程的，实际测试时会是多线程的，并且发送完之后会Kill进程，再起消费逻辑
 
@@ -50,6 +50,7 @@ public class DemoTester {
             producer.send(messagesForQueue1.get(i));
             producer.send(messagesForQueue2.get(i));
         }
+        producer.flush();
         long end = System.currentTimeMillis();
 
         long T1 = end - start;
@@ -75,10 +76,10 @@ public class DemoTester {
                 //实际测试时，会一一比较各个字段
                 if (topic != null) {
                     Assert.assertEquals(topic1, topic);
-                    Assert.assertEquals(messagesForTopic1.get(topic1Offset++), message);
+                    Assert.assertEquals(new String(((DefaultBytesMessage)messagesForTopic1.get(topic1Offset++)).getBody()), new String(((DefaultBytesMessage)message).getBody()));
                 } else {
                     Assert.assertEquals(queue1, queue);
-                    Assert.assertEquals(messagesForQueue1.get(queue1Offset++), message);
+                    Assert.assertEquals(new String(((DefaultBytesMessage)messagesForQueue1.get(queue1Offset++)).getBody()), new String(((DefaultBytesMessage)message).getBody()));
                 }
             }
             long endConsumer = System.currentTimeMillis();
@@ -110,14 +111,14 @@ public class DemoTester {
                 //实际测试时，会一一比较各个字段
                 if (topic != null) {
                     if (topic.equals(topic1)) {
-                        Assert.assertEquals(messagesForTopic1.get(topic1Offset++), message);
+                        Assert.assertEquals(new String(((DefaultBytesMessage)messagesForTopic1.get(topic1Offset++)).getBody()), new String(((DefaultBytesMessage)message).getBody()));
                     } else {
                         Assert.assertEquals(topic2, topic);
-                        Assert.assertEquals(messagesForTopic2.get(topic2Offset++), message);
+                        Assert.assertEquals(new String(((DefaultBytesMessage)messagesForTopic2.get(topic2Offset++)).getBody()), new String(((DefaultBytesMessage)message).getBody()));
                     }
                 } else {
                     Assert.assertEquals(queue2, queue);
-                    Assert.assertEquals(messagesForQueue2.get(queue2Offset++), message);
+                    Assert.assertEquals(new String(((DefaultBytesMessage)messagesForQueue2.get(queue2Offset++)).getBody()), new String(((DefaultBytesMessage)message).getBody()));
                 }
             }
             long endConsumer = System.currentTimeMillis();
