3007a9cf9c85161a5cac4e2f32a347a847be4e10
bgk <bgk083@163.com>
2017-05-22 00:13:55 +0800

    comment message store putmessage just for test

--- src/main/java/io/openmessaging/demo/MessageStore.java
+++ src/main/java/io/openmessaging/demo/MessageStore.java
@@ -13,18 +13,19 @@ public class MessageStore {
     private Map<String, HashMap<String, Integer>> queueOffsets = new HashMap<>();
 
     public void putMessage(Message message) {
-//        if (message == null) throw new ClientOMSException("Message should not be null");
-//        String topic = message.headers().getString(MessageHeader.TOPIC);
-//        String queue = message.headers().getString(MessageHeader.QUEUE);
-//        if ((topic == null && queue == null) || (topic != null && queue != null)) {
-//            throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
-//        }
-//        String bucket = topic != null ? topic : queue;
-//        // if messageBuckets don't contain specific topic or queue,
-//        // then add topic or queue to messageBuckets.
+        if (message == null) throw new ClientOMSException("Message should not be null");
+        String topic = message.headers().getString(MessageHeader.TOPIC);
+        String queue = message.headers().getString(MessageHeader.QUEUE);
+        if ((topic == null && queue == null) || (topic != null && queue != null)) {
+            throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
+        }
+        String bucket = topic != null ? topic : queue;
+        // if messageBuckets don't contain specific topic or queue,
+        // then add topic or queue to messageBuckets.
 //        if (!messageBuckets.containsKey(bucket)) {
 //            messageBuckets.put(bucket, new LinkedList<>());
 //        }
+//        // TODO speed too slow
 //        LinkedList<Message> bucketList = messageBuckets.get(bucket);
 //        bucketList.add(message);
     }
