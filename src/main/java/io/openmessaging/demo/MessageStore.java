213adc01a94e37d135f348c354b5cc2574195c47
bgk <bgk083@163.com>
2017-05-22 01:15:11 +0800

    just for test

--- src/main/java/io/openmessaging/demo/MessageStore.java
+++ src/main/java/io/openmessaging/demo/MessageStore.java
@@ -8,18 +8,28 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class MessageStore {
+
     private Map<String, LinkedList<Message>> messageBuckets = new HashMap<>();
-    private LinkedList<Message> messages = new LinkedList<>();
     private Map<String, HashMap<String, Integer>> queueOffsets = new HashMap<>();
 
+    private int i = 0;
     public void putMessage(Message message) {
-//        if (message == null) throw new ClientOMSException("Message should not be null");
-//        String topic = message.headers().getString(MessageHeader.TOPIC);
-//        String queue = message.headers().getString(MessageHeader.QUEUE);
-//        if ((topic == null && queue == null) || (topic != null && queue != null)) {
-//            throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
-//        }
-        messages.add(message);
+        if (message == null) throw new ClientOMSException("Message should not be null");
+        String topic = message.headers().getString(MessageHeader.TOPIC);
+        String queue = message.headers().getString(MessageHeader.QUEUE);
+        if ((topic == null && queue == null) || (topic != null && queue != null)) {
+            throw new ClientOMSException(String.format("Queue:%s Topic:%s should put one and only one", true, queue));
+        }
+        String bucket = topic != null ? topic : queue;
+        // if messageBuckets don't contain specific topic or queue,
+        // then add topic or queue to messageBuckets.
+        if (!messageBuckets.containsKey(bucket)) {
+            messageBuckets.put(bucket, new LinkedList<>());
+        }
+        // TODO speed too slow
+        System.out.println(bucket + ":" + (i++));
+        LinkedList<Message> bucketList = messageBuckets.get(bucket);
+        bucketList.add(message);
     }
 
 
