29cccef0c16f071fc24c339ef2812c1665dab627
bgk <bgk083@163.com>
2017-05-25 22:04:40 +0800

    add memory fix

--- src/main/java/io/openmessaging/demo/MessageDrawer.java
+++ src/main/java/io/openmessaging/demo/MessageDrawer.java
@@ -5,6 +5,7 @@ import io.openmessaging.Message;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -15,6 +16,10 @@ public class MessageDrawer {
     private LinkedList<Message> messageQueue = new LinkedList<>();
     private LinkedList<String> nonConsumeFiles = new LinkedList<>();
     private String storePath;
+    private BufferedReader currentReader;
+    // can modify to satisfy memory need.
+    // if memory is big, readCount can be bigger, vice versa.
+    private int readCount = 1024;
 
     public MessageDrawer(String storePath) {
         this.storePath = storePath;
@@ -26,10 +31,15 @@ public class MessageDrawer {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String row;
+            int num = 0;
             while ((row = reader.readLine()) != null) {
                 messageQueue.add(parseMessage(row));
+                num++;
+                if (num >= readCount) {
+                    break;
+                }
             }
-            reader.close();
+            currentReader = reader;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -43,10 +53,31 @@ public class MessageDrawer {
     public Message pullMessage() {
         Message message = messageQueue.poll();
         if (message == null) {
-            String nonConsumeFileName = nonConsumeFiles.poll();
-            if (nonConsumeFileName != null) {
-                readFile(storePath + "/" + nonConsumeFileName);
-                message = messageQueue.poll();
+            try {
+                String row;
+                int num = 0;
+                while ((row = currentReader.readLine()) != null) {
+                    messageQueue.add(parseMessage(row));
+                    num++;
+                    if (num >= readCount) {
+                        break;
+                    }
+                }
+                if (num == 0) {
+                    currentReader.close();
+                    String nonConsumeFileName = nonConsumeFiles.poll();
+                    if (nonConsumeFileName != null) {
+                        readFile(storePath + "/" + nonConsumeFileName);
+                        message = messageQueue.poll();
+                    }
+                } else if (num < readCount) {
+                    currentReader.close();
+                    message = messageQueue.poll();
+                } else {
+                    message = messageQueue.poll();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         return message;
